ac03fba633262e7255c44239e0e01dc1d7459842
EdwinCanjay <canjayedwin105@gmail.com>
2023-02-24 14:23:22 -0600

    Servicios

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+//import { ContactComponent } from './pages/contact/contact.component';
+//import { AboutComponent } from './pages/about/about.component';
+//import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RouterModule} from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { PagesModule } from './pages/pages.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+@NgModule({
+  declarations: [
+    AppComponent,
+    /*ContactComponent,
+    AboutComponent,
+    HomeComponent,*/
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    PagesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/pages/posts/posts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit{
+constructor(private dataService:DataService){}
+ngOnInit(): void {
+  this.dataService.getPosts().subscribe(
+    posts=>{
+      console.log(posts);
+    }
+  )
+}
+}
